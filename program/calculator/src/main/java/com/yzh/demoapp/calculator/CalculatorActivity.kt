deaaa125923c7686c3767de9e2b84c5e75ee9e50
eternity6666 <eternity6666@qq.com>
2021-12-08 22:26:38 +0800

    计算器

--- program/calculator/src/main/java/com/yzh/demoapp/calculator/CalculatorActivity.kt
+++ program/calculator/src/main/java/com/yzh/demoapp/calculator/CalculatorActivity.kt
@@ -1,11 +1,16 @@
 package com.yzh.demoapp.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yzh.demoapp.calculator.fragment.CalculatorFragment
 
 class CalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+        val fragment = CalculatorFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, fragment)
+            .commit()
     }
 }
