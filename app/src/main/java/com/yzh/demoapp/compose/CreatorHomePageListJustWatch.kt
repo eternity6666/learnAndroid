9bc19b4960d4285e725f7a0ac9dbc0b4c8544646
eternity6666 <eternity6666@qq.com>
2022-12-29 17:27:18 +0800

    刚刚开关 demo

---
+++ app/src/main/java/com/yzh/demoapp/compose/CreatorHomePageListJustWatch.kt
@@ -0,0 +1,50 @@
+/*
+ * CopyRight (C) 2022 Tencent. All rights reserved.
+ */
+package com.yzh.demoapp.compose
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yzh.demoapp.R
+
+@Composable
+fun CreatorHomePageListJustWatchView() {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        backgroundColor = Color.White,
+        border = BorderStroke(4.dp, Color.White.copy(alpha = 0.3f)),
+        elevation = 4.dp
+    ) {
+        Text(
+            text = stringResource(id = R.string.creator_just_watched),
+            letterSpacing = 2.sp,
+            fontSize = 14.sp,
+            lineHeight = 22.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 5.dp)
+                .offset(y = (-1.2f).dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun Preview_CreatorHomePageListJustWatchView() {
+    CreatorHomePageListJustWatchView()
+}
