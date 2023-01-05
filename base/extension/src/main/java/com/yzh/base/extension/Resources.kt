46e1e7e9d5877071e3f065e387dc93f6e2654800
eternity6666 <eternity6666@qq.com>
2023-01-05 19:50:24 +0800

    解决编译失败

---
+++ base/extension/src/main/java/com/yzh/base/extension/Resources.kt
@@ -0,0 +1,16 @@
+/*
+ * CopyRight (C) 2023 Tencent. All rights reserved.
+ */
+package com.yzh.base.extension
+
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import androidx.core.content.res.ResourcesCompat
+
+fun Resources.color(colorResId: Int): Int {
+    return ResourcesCompat.getColor(this, colorResId, null)
+}
+
+fun Resources.drawable(drawableResId: Int): Drawable? {
+    return ResourcesCompat.getDrawable(this, drawableResId, null)
+}
