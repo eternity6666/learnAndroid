d59690f3a686165fb62eb1fc028a504ec22875c5
eternity6666 <eternity6666@qq.com>
2023-03-16 17:27:42 +0800

    新增 YActivity.kt

--- base/annotation/src/main/java/com/yzh/annotation/YActivity.kt
+++ base/annotation/src/main/java/com/yzh/annotation/YActivity.kt
@@ -8,7 +8,7 @@ package com.yzh.annotation
  * @since 2023/3/16 16:16
  */
 @Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.SOURCE)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class YActivity(
     val title: String = "",
     val description: String = "",
