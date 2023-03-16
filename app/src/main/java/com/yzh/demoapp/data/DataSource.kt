a86d162a012c64df5a0d33a92f309bd416a2a806
eternity6666 <eternity6666@qq.com>
2023-03-16 18:57:40 +0800

    YActivity

--- app/src/main/java/com/yzh/demoapp/data/DataSource.kt
+++ app/src/main/java/com/yzh/demoapp/data/DataSource.kt
@@ -2,18 +2,20 @@ package com.yzh.demoapp.data
 
 import android.app.Activity
 import com.yzh.annotation.YActivity
+import com.yzh.demo.card_view.CardViewActivity
 import com.yzh.demo.recycler_view.RecyclerViewActivity
 import com.yzh.demoapp.activity.CameraXDemoActivity
+import com.yzh.demoapp.activity.ComposeLearnActivity
 import com.yzh.demoapp.activity.CustomViewActivity
 import com.yzh.demoapp.activity.GradientDrawableActivity
 import com.yzh.demoapp.activity.OrientationActivity
 import com.yzh.demoapp.activity.ValueAnimatorActivity
 import com.yzh.demoapp.activity.WeatherActivity
+import com.yzh.demoapp.calculator.CalculatorActivity
 import kotlin.reflect.KClass
 
 object DataSource {
 
-    private val dataList: ArrayList<MainPageItemData> = ArrayList()
     private val activityList = listOf<KClass<out Activity>>(
         WeatherActivity::class,
         CameraXDemoActivity::class,
@@ -22,24 +24,27 @@ object DataSource {
         ValueAnimatorActivity::class,
         RecyclerViewActivity::class,
         GradientDrawableActivity::class,
+        CardViewActivity::class,
+        CalculatorActivity::class,
+        ComposeLearnActivity::class,
     )
-
-    init {
-        activityList.forEach { clazz ->
-            val annotation = clazz.annotations.find { it is YActivity } as? YActivity
-            val description = annotation?.description.orEmpty()
-            val title = annotation?.title ?: clazz.simpleName.orEmpty()
-            dataList.add(
-                MainPageItemData(
-                    title = title,
-                    description = description,
-                    clazz = clazz,
-                )
-            )
-        }
-    }
+    private val dataList: ArrayList<MainPageItemData> = ArrayList()
 
     fun getMainPageList(): ArrayList<MainPageItemData> {
+        if (dataList.isEmpty()) {
+            activityList.forEach { clazz ->
+                val annotation = clazz.annotations.find { it is YActivity } as? YActivity
+                val title = annotation?.title ?: clazz.simpleName.orEmpty()
+                val description = annotation?.description.orEmpty()
+                dataList.add(
+                    MainPageItemData(
+                        title = title,
+                        description = description,
+                        clazz = clazz,
+                    )
+                )
+            }
+        }
         return dataList
     }
 }
