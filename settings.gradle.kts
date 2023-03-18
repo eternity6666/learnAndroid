1e5de2967395579e6f6f85c65f86a01b44f6ea4f
eternity6666 <eternity6666@qq.com>
2023-03-18 20:14:17 +0800

    maven 库替换为 aliyun

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,12 @@ enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/spring")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/spring-plugin")
         google()
         mavenCentral()
     }
