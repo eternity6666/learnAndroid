368821dcdd0169054d980cda06d74acfcdbda1d4
eternity6666 <eternity6666@qq.com>
2023-03-21 00:41:49 +0800

    AppListActivity.kt

---
+++ app/src/main/java/com/yzh/demoapp/activity/AppListActivity.kt
@@ -0,0 +1,35 @@
+package com.yzh.demoapp.activity
+
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import com.yzh.demoapp.ui.theme.LearnAndroidTheme
+
+
+class AppListActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val list = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            packageManager.getInstalledApplications(PackageManager.ApplicationInfoFlags.of(0L))
+        } else {
+            packageManager.getInstalledApplications(0)
+        }
+        setContent {
+            LearnAndroidTheme {
+                LazyColumn {
+                    items(list) {
+                        Card {
+                            Text(text = it?.packageName.orEmpty())
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
