5bcaebe4e0ce09892247ab9c161adbbcb00ba93d
eternity6666 <eternity6666@qq.com>
2023-03-21 09:43:37 +0800

    AppListActivity.kt

--- app/src/main/java/com/yzh/demoapp/activity/AppListActivity.kt
+++ app/src/main/java/com/yzh/demoapp/activity/AppListActivity.kt
@@ -5,10 +5,13 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 import com.yzh.demoapp.ui.theme.LearnAndroidTheme
 
 
@@ -16,16 +19,23 @@ class AppListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val list = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            packageManager.getInstalledApplications(PackageManager.ApplicationInfoFlags.of(0L))
+            packageManager.getInstalledPackages(PackageManager.PackageInfoFlags.of(0L))
         } else {
-            packageManager.getInstalledApplications(0)
+            packageManager.getInstalledPackages(0)
+        }
+        list.sortBy {
+            it.applicationInfo.loadLabel(packageManager).toString()
         }
         setContent {
             LearnAndroidTheme {
                 LazyColumn {
                     items(list) {
-                        Card {
-                            Text(text = it?.packageName.orEmpty())
+                        Card(modifier = Modifier.fillMaxWidth()) {
+                            Row {
+                                Text(text = it?.packageName.orEmpty())
+                                val bitmap = it?.applicationInfo?.loadIcon(packageManager)
+
+                            }
                         }
                     }
                 }
