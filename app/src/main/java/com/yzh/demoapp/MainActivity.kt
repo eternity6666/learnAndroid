a13ff4f494d7046b87512397ff62ba54a0d4786a
eternity6666 <eternity6666@qq.com>
2023-04-11 20:53:32 +0800

    使用 Navigation Compose 重新组织界面

--- app/src/main/java/com/yzh/demoapp/MainActivity.kt
+++ app/src/main/java/com/yzh/demoapp/MainActivity.kt
@@ -3,10 +3,6 @@ package com.yzh.demoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.yzh.demoapp.ui.theme.LearnAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,13 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearnAndroidTheme {
-                HomePage(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(all = 16.dp)
-                )
+                MainPage()
             }
         }
     }
-
 }
---
+++ app/src/main/java/com/yzh/demoapp/MainPage.kt
@@ -0,0 +1,95 @@
+/*
+ * CopyRight (C) 2023 Tencent. All rights reserved.
+ */
+package com.yzh.demoapp
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.yzh.demoapp.compose.CubicBezierPage
+import java.io.File
+import java.io.FileOutputStream
+import java.util.zip.ZipOutputStream
+
+private sealed class Screen(
+    val route: String,
+    @StringRes val resourceId: Int,
+) {
+    object Home : Screen("home", R.string.home)
+    object Tool : Screen("tool", R.string.tool)
+}
+
+private val items = listOf(
+    Screen.Home,
+    Screen.Tool,
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainPage() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                items.forEach { screen ->
+                    NavigationBarItem(
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
+                        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                        label = { Text(stringResource(screen.resourceId)) },
+                    )
+                }
+
+            }
+        }
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(it),
+        ) {
+            composable(Screen.Home.route) {
+                HomePage(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(all = 16.dp)
+                )
+            }
+            composable(Screen.Tool.route) { CubicBezierPage() }
+        }
+    }
+
+    val x = ZipOutputStream(FileOutputStream(File("")))
+    x.use {
+
+    }
+}
