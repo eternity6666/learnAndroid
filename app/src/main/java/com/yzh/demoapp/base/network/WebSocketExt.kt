bd17d947f9efe6914e2e51548718ee2f29bc84fb
eternity6666 <eternity6666@qq.com>
2023-06-09 15:55:47 +0800

    WebSocketExt.kt

--- app/src/main/java/com/yzh/demoapp/base/network/WebSocketExt.kt
+++ app/src/main/java/com/yzh/demoapp/base/network/WebSocketExt.kt
@@ -3,3 +3,23 @@
  */
 package com.yzh.demoapp.base.network
 
+import okhttp3.WebSocket
+
+private var subscribeId = 0
+
+fun WebSocket.subscribe(destination: String) {
+    subscribeId += 1
+    stomp(StompType.Subscribe, "id:sub-$subscribeId\ndestination:$destination")
+}
+
+private fun WebSocket.stomp(type: StompType, message: String) {
+    send("${type.name}\n$message\n\n\u0000")
+}
+
+private sealed class StompType(val name: String) {
+    object Connect : StompType("CONNECT")
+    object Begin: StompType("BEGIN")
+    object Subscribe : StompType("SUBSCRIBE")
+    object Commit: StompType("COMMIT")
+    object ACK: StompType("ACK")
+}
