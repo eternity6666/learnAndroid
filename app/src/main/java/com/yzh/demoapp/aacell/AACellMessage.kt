38becdc0ebd23f6a4ef27ee01972fe3d79581606
eternity6666 <eternity6666@qq.com>
2023-06-09 15:28:58 +0800

    AACellMessage.kt

---
+++ app/src/main/java/com/yzh/demoapp/aacell/AACellMessage.kt
@@ -0,0 +1,20 @@
+package com.yzh.demoapp.aacell
+
+import com.yzh.demoapp.base.data.toTyped
+
+/**
+ * CopyRight (C) 2023 Tencent. All rights reserved.
+ *
+ * @author baronyang@tencent.com
+ * @since 2023/6/9 16:16
+ */
+data class AACellMessage(
+    val message: String = "",
+    val id: Int = 0,
+    val isDelete: Boolean = false,
+    val time: Long = 0L,
+)
+
+fun AACellMessage(json: String): AACellMessage? {
+    return json.toTyped()
+}
