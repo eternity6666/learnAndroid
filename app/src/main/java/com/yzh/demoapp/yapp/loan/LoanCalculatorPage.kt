786d5d4c8d26fa7643abe8aadc39abcae8108452
eternity6666 <eternity6666@qq.com>
2023-07-25 14:59:07 +0800

    等本等息计算器

--- app/src/main/java/com/yzh/demoapp/yapp/loan/LoanCalculatorPage.kt
+++ app/src/main/java/com/yzh/demoapp/yapp/loan/LoanCalculatorPage.kt
@@ -1,21 +1,60 @@
 package com.yzh.demoapp.yapp.loan
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoanCalculatorPage() {
-    val originPrice = remember {
-        mutableStateOf(0.00)
+    val loanAmount = remember {
+        mutableStateOf(100000.00)
     }
-    Box(modifier = Modifier.fillMaxSize()) {
-        TextField(value = originPrice.value.toString(), onValueChange = {
-            originPrice.value = it.toDoubleOrNull() ?: 0.0
-        })
+    val interestRate = remember {
+        mutableStateOf(3.50)
+    }
+    val numberOfLoad = remember {
+        mutableStateOf(24)
+    }
+    val type = remember {
+        mutableStateOf<LoanType>(LoanType.SamePrincipalSameInterest)
+    }
+    val result = type.value.calculator(loanAmount.value, interestRate.value, numberOfLoad.value)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        TextField(
+            value = loanAmount.value.toString(),
+            onValueChange = {
+                loanAmount.value = it.toDoubleOrNull() ?: 0.0
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        result.forEachIndexed { index, (principalAndInterest, principal, interest) ->
+            if (index == 0) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "期数", modifier = Modifier.weight(1f))
+                    Text(text = "月供(元)", modifier = Modifier.weight(1f))
+                    Text(text = "本金(元)", modifier = Modifier.weight(1f))
+                    Text(text = "利息(元)", modifier = Modifier.weight(1f))
+                }
+            }
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "第${index + 1}期", modifier = Modifier.weight(1f))
+                Text(text = String.format("%.2f", principalAndInterest), modifier = Modifier.weight(1f))
+                Text(text = String.format("%.2f", principal), modifier = Modifier.weight(1f))
+                Text(text = String.format("%.2f", interest), modifier = Modifier.weight(1f))
+            }
+        }
     }
 }
--- app/src/main/java/com/yzh/demoapp/yapp/loan/LoanType.kt
+++ app/src/main/java/com/yzh/demoapp/yapp/loan/LoanType.kt
@@ -2,32 +2,40 @@ package com.yzh.demoapp.yapp.loan
 
 sealed class LoanType(
     val name: String,
-    val calculator: (Double, Double, Int) -> List<Double>
+    val calculator: (Double, Double, Int) -> List<Triple<Double, Double, Double>>,
 ) {
     object SamePrincipalSameInterest : LoanType(
         name = "等本等息",
-        calculator = { loanAmount, interest, numberOfLoad ->
-            emptyList()
+        calculator = { loanAmount, interestRate, numberOfLoad ->
+            val totalInterest = loanAmount * interestRate * 0.01 * numberOfLoad / 12
+            val result = mutableListOf<Triple<Double, Double, Double>>()
+            for (i in 1..numberOfLoad) {
+                val principal = loanAmount / numberOfLoad
+                val interest = totalInterest / numberOfLoad
+                val principalAndInterest = principal + interest
+                result.add(Triple(principalAndInterest, principal, interest))
+            }
+            result
         }
     )
 
     object SameSumOfPrincipalAndInterest : LoanType(
         name = "等额本息",
-        calculator = { loanAmount, interest, numberOfLoad ->
+        calculator = { loanAmount, interestRate, numberOfLoad ->
             emptyList()
         }
     )
 
     object SamePrincipal : LoanType(
         name = "等额本金",
-        calculator = { loanAmount, interest, numberOfLoad ->
+        calculator = { loanAmount, interestRate, numberOfLoad ->
             emptyList()
         }
     )
 
     object FirstInterestThenPrincipal : LoanType(
         name = "先息后本",
-        calculator = { loanAmount, interest, numberOfLoad ->
+        calculator = { loanAmount, interestRate, numberOfLoad ->
             emptyList()
         }
     )
