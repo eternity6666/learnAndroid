29f58a1a648aef7a86835fde660ee36d4412facc
eternity6666 <eternity6666@qq.com>
2023-07-27 15:09:42 +0800

    ContentProvider

---
+++ app/src/main/java/com/yzh/demoapp/DemoAppContentProvider.kt
@@ -0,0 +1,54 @@
+/*
+ * CopyRight (C) 2023 Tencent. All rights reserved.
+ */
+package com.yzh.demoapp
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+/**
+ * @author baronyang@tencent.com
+ * @since 2023/7/27 16:16
+ */
+class DemoAppContentProvider : ContentProvider() {
+    override fun onCreate(): Boolean {
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?,
+    ): Cursor? {
+        return null
+    }
+
+    override fun getType(uri: Uri): String? {
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+    ): Int {
+        return 0
+    }
+
+    companion object {
+        private const val TAG: String = "DemoAppContentProvider"
+    }
+}
