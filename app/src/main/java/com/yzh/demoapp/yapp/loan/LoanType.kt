e919d915326f8d280f6e87762f9db9a6b79d217d
eternity6666 <eternity6666@qq.com>
2023-07-27 12:08:16 +0800

    实现计算逻辑

--- app/src/main/java/com/yzh/demoapp/yapp/loan/LoanType.kt
+++ app/src/main/java/com/yzh/demoapp/yapp/loan/LoanType.kt
@@ -1,19 +1,23 @@
 package com.yzh.demoapp.yapp.loan
 
+import kotlin.math.pow
+
 sealed class LoanType(
     val name: String,
     val calculator: (Double, Double, Int) -> List<Triple<Double, Double, Double>>,
 ) {
     data object SamePrincipalSameInterest : LoanType(
         name = "等本等息",
-        calculator = { loanAmount, interestRate, numberOfLoad ->
-            val totalInterest = loanAmount * interestRate * 0.01 * numberOfLoad / 12
+        calculator = { loanAmount, interestYearRate, numberOfLoad ->
             val result = mutableListOf<Triple<Double, Double, Double>>()
-            for (i in 1..numberOfLoad) {
-                val principal = loanAmount / numberOfLoad
-                val interest = totalInterest / numberOfLoad
-                val principalAndInterest = principal + interest
-                result.add(Triple(principalAndInterest, principal, interest))
+            if (numberOfLoad > 0) {
+                val interestMonthRate = interestYearRate * 0.01 / 12
+                for (i in 1..numberOfLoad) {
+                    val principal = loanAmount / numberOfLoad
+                    val interest = loanAmount * interestMonthRate
+                    val principalAndInterest = principal + interest
+                    result.add(Triple(principalAndInterest, principal, interest))
+                }
             }
             result
         }
@@ -21,22 +25,57 @@ sealed class LoanType(
 
     data object SameSumOfPrincipalAndInterest : LoanType(
         name = "等额本息",
-        calculator = { loanAmount, interestRate, numberOfLoad ->
-            emptyList()
+        calculator = { loanAmount, interestYearRate, numberOfLoad ->
+            val result = mutableListOf<Triple<Double, Double, Double>>()
+            if (numberOfLoad > 0) {
+                val interestMonthRate = interestYearRate * 0.01 / 12
+                var remainingLoanAmount= loanAmount
+                for (i in 1..numberOfLoad) {
+                    val onePlusRatePowN = (1 + interestMonthRate).pow(numberOfLoad)
+                    val total = loanAmount * (interestMonthRate * onePlusRatePowN) / (onePlusRatePowN - 1)
+                    val interest = remainingLoanAmount * interestMonthRate
+                    val principal = total - interest
+                    remainingLoanAmount -= principal
+                    result.add(Triple(total, principal, interest))
+                }
+            }
+            result
         }
     )
 
     data object SamePrincipal : LoanType(
         name = "等额本金",
-        calculator = { loanAmount, interestRate, numberOfLoad ->
-            emptyList()
+        calculator = { loanAmount, interestYearRate, numberOfLoad ->
+            val result = mutableListOf<Triple<Double, Double, Double>>()
+            if (numberOfLoad > 0) {
+                val interestMonthRate = interestYearRate * 0.01 / 12
+                for (i in 1..numberOfLoad) {
+                    val principal = loanAmount / numberOfLoad
+                    val interest = loanAmount * (numberOfLoad - i + 1) * interestMonthRate / numberOfLoad
+                    result.add(Triple(principal + interest, principal, interest))
+                }
+            }
+            result
         }
     )
 
     data object FirstInterestThenPrincipal : LoanType(
         name = "先息后本",
-        calculator = { loanAmount, interestRate, numberOfLoad ->
-            emptyList()
+        calculator = { loanAmount, interestYearRate, numberOfLoad ->
+            val result = mutableListOf<Triple<Double, Double, Double>>()
+            if (numberOfLoad > 0) {
+                val interestMonthRate = interestYearRate * 0.01 / 12
+                for (index in 1..numberOfLoad) {
+                    val principal = if (index == numberOfLoad) {
+                        loanAmount
+                    } else {
+                        0.0
+                    }
+                    val interest = loanAmount * interestMonthRate
+                    result.add(Triple(principal + interest, principal, interest))
+                }
+            }
+            result
         }
     )
 }
